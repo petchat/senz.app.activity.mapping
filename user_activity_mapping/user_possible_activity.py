5cbb9f2a2cdf2c89a46b19ee910dced60690786d
kevin <tutengfei.kevin@gmail.com>
2015-12-29 16:10:33 +0800

    fixed mapping user activity use pyleus

--- user_activity_mapping/user_possible_activity.py
+++ user_activity_mapping/user_possible_activity.py
@@ -3,13 +3,13 @@
 
 from __future__ import absolute_import, division
 
+import json
 import logging
 
 from pyleus.storm import SimpleBolt
 
 from user_activity_mapping.session import redis_location_session
 from user_activity_mapping.session import user_activity_session
-
 from user_activity_mapping.util import get_events_from_mongodb_by_coordinates
 
 log = logging.getLogger('user_possible_activity')
@@ -27,14 +27,16 @@ class UpaBolt(SimpleBolt):
         log.debug('--UpaBolt--get-location--:' +
                   str(_raw_data_str['location']))
 
-        user_location = eval(_raw_data_str['location'])
+        user_location = json.loads(_raw_data_str['location'])
 
         user_id = user_location['user_id']
+        log.debug('--UpaBolt--get-user_id--:' + str(user_id))
 
         _timestamp = user_location['timestamp']
+        log.debug('--UpaBolt--get-timestamp--:' + str(_timestamp))
 
-        coordinates = [user_location['longitude'],
-                       user_location['latitude']]
+        coordinates = [user_location['location']['lng'],
+                       user_location['location']['lat']]
 
         # get possible activity
         possible_acts = get_events_from_mongodb_by_coordinates(
@@ -44,6 +46,8 @@ class UpaBolt(SimpleBolt):
 
         # 如果有可能的活动传出去 准备多次匹配
         if possible_acts:
+            log.debug('--UpaBolt--user_id--possible_activities--: %s----%s'
+                      % (user_id, possible_acts))
             # save current location to db and possible acts
             redis_location_session.setex(user_location['_id'],
                                          3600 * 2,
